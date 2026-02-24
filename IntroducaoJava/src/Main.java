cb594bf0eec495485d2e0a8278fda2c4d96d7d9f
Daniel <dfpires@gmail.com>
2026-02-24 11:29:41 -0300

    exemplo 1 com if else e Scanner

--- IntroducaoJava/src/Main.java
+++ IntroducaoJava/src/Main.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
+        // solicita entrada de dados
+        Scanner entrada = new Scanner(System.in);
         // variável do tipo String
-        String nome = "Daniel";
+        System.out.println("Informe seu nome");
+        String nome = entrada.next();
         // variável do tipo int
-        int idade = 51;
+        System.out.println("Informe a idade");
+        int idade = entrada.nextInt();
         // variável do tipo float
-        float peso = 92.5f;
-        float altura = 1.78f;
+        System.out.println("Informe o peso");
+        float peso = entrada.nextFloat();
+        System.out.println("Informe a altura");
+        float altura = entrada.nextFloat();
         // calcula o IMC
         float imc = peso / (altura * altura);
         System.out.println( nome + " seu IMC é " + imc);
-        if (imc < 18.5f) {
-            System.out.println("Você está abaixo do peso");
+        String mensagem = "";
+        // operador lógico E em java é representado por &&
+        if (imc >= 0 && imc < 18.5f) {
+           mensagem = "Abaixo do peso";
         }
+        else if (imc >= 18.5f && imc <= 24.9f){
+            mensagem = "Peso normal";
+        }
+        else if (imc >= 25 && imc <= 29.9f){
+            mensagem = "Sobrepeso";
+        }
+        else if (imc >= 30 && imc <= 34.9f){
+            mensagem = "Obesidade grau I";
+        }
+        else if (imc >= 35 && imc <= 39.9f){
+            mensagem = "Obesidade grau II";
+        }
+        else if (imc >= 40){
+            mensagem = "Obesidade grau III";
+        }
+        else
+            mensagem = "Valores inválidos";
+
+        System.out.println(nome + " sua classificação é " + mensagem);
     }
 }
