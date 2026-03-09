fad6ba2ed02f3077189bc8d8a69e74322e03719e
Daniel <dfpires@gmail.com>
2026-03-09 11:03:34 -0300

    comando while com break e continue

--- IntroducaoJava/src/Calculadora.java
+++ IntroducaoJava/src/Calculadora.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Calculadora {
     public static void main(String[] args){
-
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe um número");
+        float n1 = entrada.nextFloat();
+        System.out.println("Informe outro número");
+        float n2 = entrada.nextFloat();
+        System.out.println("Informe o operador +, -, * ou /");
+        String operador = entrada.next();
+        float resultado = 0;
+        switch(operador) {
+            case "+": resultado = n1 + n2; break;
+            case "-": resultado = n1 - n2; break;
+            case "*": resultado = n1 * n2; break;
+            case "/": resultado = n1 / n2; break;
+            default: System.out.println("Operador inválido"); return;
+        }
+        System.out.println("Resultado: " + resultado);
     }
 }
